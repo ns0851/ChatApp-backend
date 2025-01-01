baf357e5a21c1e50901e10c6f6e1017894aabbc2
Satyadev <126260629+ns0851@users.noreply.github.com>
2025-01-01 20:32:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,19 +12,8 @@ import bodyParser from "body-parser";
 const app = express();
 const PORT = process.env.PORT;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://hows-app.vercel.app');
-  next();
-});
-
-app.use(
-  cors({
-    origin: "https://hows-app.vercel.app",
-    credentials: true,
-  })
-);
-
 app.use(express.json());
+app.use(cors());
 app.use(express.json({ limit: "100mb", extended: true }));
 app.use(
   express.urlencoded({ limit: "100mb", extended: true, parameterLimit: 50000 })
