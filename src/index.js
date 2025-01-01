9c0720f9281d54489c3609569e604edbc8e885cc
Satyadev <126260629+ns0851@users.noreply.github.com>
2025-01-01 19:58:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,14 @@ import bodyParser from "body-parser";
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 app.use(express.json({ limit: "100mb", extended: true }));
